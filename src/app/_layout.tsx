7e20be41a9e359e931c0cf97ef6137da131fa4d3
Tejpreet12 <tejpreetdhiman429@gmail.com>
2025-08-20 23:43:32 +0530

    Sqlite-added

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,25 +1,64 @@
 import { Drawer } from "expo-router/drawer";
+import { SQLiteDatabase, SQLiteProvider } from "expo-sqlite";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
   return (
-    <GestureHandlerRootView>
-      <Drawer>
-        <Drawer.Screen name="(tabs)" options={{ title: "Main Tabs" }} />
-        <Drawer.Screen
-          name="profile"
-          options={{
-            title: "Profile",
-            drawerItemStyle: { display: "none" },
-          }}
-        />
-        <Drawer.Screen
-          name="index"
-          options={{
-            drawerItemStyle: { display: "none" },
-          }}
-        />
-      </Drawer>
-    </GestureHandlerRootView>
+    <SQLiteProvider databaseName="test.db" onInit={migrateDbIfNeeded}>
+      <GestureHandlerRootView>
+        <Drawer>
+          <Drawer.Screen name="(tabs)" options={{ title: "Main Tabs" }} />
+          <Drawer.Screen
+            name="profile"
+            options={{
+              title: "Profile",
+              drawerItemStyle: { display: "none" },
+            }}
+          />
+          <Drawer.Screen
+            name="index"
+            options={{
+              drawerItemStyle: { display: "none" },
+            }}
+          />
+        </Drawer>
+      </GestureHandlerRootView>
+    </SQLiteProvider>
   );
+}
+
+async function migrateDbIfNeeded(db: SQLiteDatabase) {
+  console.log("migrateDbIfNeeded")
+  const DATABASE_VERSION = 1;
+  const result = await db.getFirstAsync<{ user_version: number }>(
+    "PRAGMA user_version"
+  );
+  let currentDbVersion = result?.user_version ?? 0;
+  if (currentDbVersion >= DATABASE_VERSION) {
+    return;
+  }
+  if (currentDbVersion === 0) {
+    await db.execAsync(`
+PRAGMA journal_mode = 'wal';
+CREATE TABLE todos (id INTEGER PRIMARY KEY NOT NULL, value TEXT NOT NULL, intValue INTEGER);
+`);
+    await db.runAsync(
+      "INSERT INTO todos (value, intValue) VALUES (?, ?)",
+      "hello",
+      1
+    );
+    await db.runAsync(
+      "INSERT INTO todos (value, intValue) VALUES (?, ?)",
+      "world",
+      2
+    );
+    currentDbVersion = 1;
+  }
+  if (currentDbVersion === 1) {
+    // Add more migrations
+    await db.execAsync(`
+      ALTER TABLE tasks ADD COLUMN imageUri TEXT`);
+      currentDbVersion = 2;
+  }
+  await db.execAsync(`PRAGMA user_version = ${DATABASE_VERSION}`);
 }
