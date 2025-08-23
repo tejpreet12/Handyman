887d59efe91fae737c202bfe01b130f209f4aefa
Tejpreet12 <tejpreetdhiman429@gmail.com>
2025-08-24 00:38:23 +0530

    crud-sqlite

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -28,20 +28,25 @@ export default function RootLayout() {
 }
 
 async function migrateDbIfNeeded(db: SQLiteDatabase) {
-  console.log("migrateDbIfNeeded")
+  console.log("migrateDbIfNeeded");
   const DATABASE_VERSION = 1;
   const result = await db.getFirstAsync<{ user_version: number }>(
     "PRAGMA user_version"
   );
   let currentDbVersion = result?.user_version ?? 0;
+  console.log(currentDbVersion, "CURRENT DB VERSION");
   if (currentDbVersion >= DATABASE_VERSION) {
     return;
   }
+
   if (currentDbVersion === 0) {
     await db.execAsync(`
-PRAGMA journal_mode = 'wal';
-CREATE TABLE todos (id INTEGER PRIMARY KEY NOT NULL, value TEXT NOT NULL, intValue INTEGER);
-`);
+    PRAGMA journal_mode = 'wal';
+      CREATE TABLE IF NOT EXISTS todos (
+          id INTEGER PRIMARY KEY NOT NULL, 
+          value TEXT NOT NULL, 
+          intValue INTEGER
+        );    `);
     await db.runAsync(
       "INSERT INTO todos (value, intValue) VALUES (?, ?)",
       "hello",
@@ -56,9 +61,8 @@ CREATE TABLE todos (id INTEGER PRIMARY KEY NOT NULL, value TEXT NOT NULL, intVal
   }
   if (currentDbVersion === 1) {
     // Add more migrations
-    await db.execAsync(`
-      ALTER TABLE tasks ADD COLUMN imageUri TEXT`);
-      currentDbVersion = 2;
+    await db.execAsync(`ALTER TABLE todos ADD COLUMN imageUri TEXT`);
+    currentDbVersion = 2;
   }
-  await db.execAsync(`PRAGMA user_version = ${DATABASE_VERSION}`);
+  await db.execAsync(`PRAGMA user_version = ${currentDbVersion}`);
 }
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,14 +1,101 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-
-const Home = () => {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { useSQLiteContext } from "expo-sqlite";
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+interface Todo {
+  id: number;
+  imageUri: string | null;
+  intValue: number;
+  value: string;
 }
 
-export default Home
+const Home = () => {
+  const db = useSQLiteContext();
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [newTodo, setNewTodo] = useState("");
 
-const styles = StyleSheet.create({})
+  const getTodos = async () => {
+    const result = await db.getAllAsync<Todo>("SELECT * FROM todos");
+    setTodos(result);
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  const addTodo = async (todo: string, intValue: number) => {
+    await db.runAsync(
+      "INSERT INTO todos (value, intValue) VALUES (?, ?)",
+      todo,
+      intValue
+    );
+  };
+
+  const handleAddTodo = async () => {
+    await addTodo(newTodo, Math.floor(Math.random() * 100));
+    setNewTodo("");
+    getTodos();
+  };
+
+  const deleteTodo = async (id: number) => {
+    await db.runAsync("DELETE FROM todos WHERE id = ?", id);
+    getTodos();
+  };
+
+  return (
+    <View>
+      <View style={styles.container}>
+        <TextInput
+          placeholder="Add Todo"
+          style={styles.input}
+          onChangeText={(text) => setNewTodo(text)}
+          value={newTodo}
+        />
+        <Button title="Add Todo" onPress={handleAddTodo} />
+      </View>
+      <FlatList
+        data={todos}
+        renderItem={({ item }) => (
+          <View style={styles.todoItem}>
+            <Text>{item.value}</Text>
+            <TouchableOpacity onPress={() => deleteTodo(item.id)}>
+              <MaterialIcons name="delete-outline" size={24} color="black" />
+            </TouchableOpacity>
+          </View>
+        )}
+        keyExtractor={(item) => item.id.toString()}
+      />
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    padding: 10,
+  },
+  input: {
+    width: 250,
+    borderWidth: 1,
+    marginRight: 15,
+    borderRadius: 5,
+    padding: 8,
+  },
+  todoItem: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+    justifyContent: "space-between",
+    flexDirection: "row",
+  },
+});
